013216b43cdc53df16bfd5d7c212a257788e0adb
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-O0VQQ4U>
2024-10-29 13:45:47 +0300

    Connect all to program.cs

--- LastHope.Web/Program.cs
+++ LastHope.Web/Program.cs
@@ -1,10 +1,19 @@
+using LastHope.Application.DTO;
+using LastHope.Application.Mapping;
+using LastHope.Application.Service;
+using LastHope.Domain.Entities;
+using LastHope.Domain.Interfaces;
 using LastHope.Infrastructure.Data;
+using LastHope.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<GenericInterface<Product>, ProductRepository>();
+builder.Services.AddScoped<GenericInterface<ProductDTO>, ProductService>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddDbContext<LastHopeDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("CS"));
